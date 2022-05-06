3b80ba93cb1e1f77ff03b8d7d694e0efcd9cd24c
cornnick <ntcorn@ualr.edu>
2022-05-06 02:03:25 +0000

    river level and portfolio start

---
+++ final/assets/js/scripts.js
@@ -0,0 +1,25 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  $(document).ready(function() {
+
+    $('.fade').slick({
+      dots: false,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
+
+
+});
